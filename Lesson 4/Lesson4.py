c143c61036e03cac252146c6dd9b506322567b46
trungsliver <buiductrung.cva@gmail.com>
2024-10-26 13:50:21 +0700

    Lesson 4

---
+++ Lesson 4/Lesson4.py
@@ -0,0 +1,10 @@
+# Tên file: [Lớp]_[HoTen]_CP1.py
+# Ví dụ: PTA12_DucTrung_CP1.py
+
+# ========= Trừ điểm =========
+# Khải Hưng: 2
+# Bảo Nam: 1
+# Minh Tâm: 2
+# Minh Đức:
+# Minh Hải:
+# Khoa Nguyên: 2
